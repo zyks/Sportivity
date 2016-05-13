d113ffc41c676bdb29ccb9e2fda07f412239fb92
zyks <pazy011@gmail.com>
2016-05-13 16:44:46 +0200

    [#4] Add UserController

---
+++ Sportivity/Scenes/User/UserController.swift
@@ -0,0 +1,24 @@
+//
+//  UserController.swift
+//  Sportivity
+//
+//  Created by Paweł Zykowski on 13.05.2016.
+//  Copyright © 2016 Paweł Zykowski. All rights reserved.
+//
+
+import UIKit
+
+
+class UserController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}

3b7734d2d55eb1f1d5341e160235f15ba14a2fc6
zyks <pazy011@gmail.com>
2016-05-13 10:11:51 +0200

    [#1] Add Objective-C bridging header

---
+++ Sportivity/Sportivity-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Sportivity-Bridging-Header.h
+//  Sportivity
+//
+//  Created by Paweł Zykowski on 13.05.2016.
+//  Copyright © 2016 Paweł Zykowski. All rights reserved.
+//
+
+#ifndef Sportivity_Bridging_Header_h
+#define Sportivity_Bridging_Header_h
+
+#import <KinveyKit/KinveyKit.h>
+
+#endif /* Sportivity_Bridging_Header_h */

8b605e20baede571d38f974e4d697d9f5416c7c9
zyks <pazy011@gmail.com>
2016-05-14 20:16:16 +0200

    [#6][#7] Add progress bar showing loading activities status and components fade in/out animations

--- Sportivity/Scenes/User/UserViewController.swift
+++ Sportivity/Scenes/User/UserViewController.swift
@@ -13,6 +13,17 @@ class UserViewController: UIViewController {
     
     @IBOutlet weak var helloLabel: UILabel!
     @IBOutlet weak var logOutButton: UIButton!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    var progress: Float = 0.0 {
+        didSet {
+            progressBar.setProgress(self.progress, animated: true)
+            if self.progress == 1.0 {
+                self.fadeOutProgressBar()
+                self.fadeInComponents()
+            }
+        }
+    }
     var usersWorker: UsersWorker?
     var activitiesWorker: ActivitiesWorker?
     var currentUser: User?
@@ -22,6 +33,8 @@ class UserViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        self.helloLabel.alpha = 0.0
+        self.logOutButton.alpha = 0.0
         self.helloLabel.text = "Hello, \(currentUser!.name)!"
         
         self.activitiesWorker = ActivitiesWorker(
@@ -30,7 +43,12 @@ class UserViewController: UIViewController {
                 withParams: ["collection": "Activities", "entityTemplate": ActivityEntity.self]
             )
         )
-        self.activitiesWorker!.loadActivities(of: currentUser!.name, andCallFunction: storeLoadedActivities)
+        
+        self.activitiesWorker!.loadActivities(
+            of: currentUser!.name,
+            reportProgressWith: { self.progress = $0 },
+            andWhenDone: storeLoadedActivities
+        )
     }
     
     func storeLoadedActivities(activities: [Activity]) {
@@ -38,6 +56,29 @@ class UserViewController: UIViewController {
         //for activity in self.userActivities {
         //    NSLog("\(activity.type), \(activity.startsAt), \(activity.endsAt)")
         //}
+    }
+    
+    func fadeOutProgressBar() {
+        UIView.animateWithDuration(
+            0.5,
+            delay: 1.0,
+            options: [],
+            animations: { self.progressBar.alpha = 0 },
+            completion: nil
+        )
+    }
+    
+    func fadeInComponents() {
+        UIView.animateWithDuration(
+            0.75,
+            delay: 1.0,
+            options: [],
+            animations: {
+                self.helloLabel.alpha = 1.0
+                self.logOutButton.alpha = 1.0
+            },
+            completion: nil
+        )
     }
     
     @IBAction func logOutTouchUpInside(sender: AnyObject) {
